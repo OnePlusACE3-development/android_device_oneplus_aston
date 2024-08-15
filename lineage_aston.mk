2ae88ec3d824f79a03a9bf5d0f6f13582db49cf8
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2024-08-15 13:36:20 +0800

    aston: Bringup Rising

--- lineage_aston.mk
+++ lineage_aston.mk
@@ -30,4 +30,42 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
+# friendly tip: builders can use vendor init_xxx.cpp as workaround for spacing
+# e.g. property_override("ro.rising.chipset", "Snapdragon 870 5G");
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 8 Gen 2"
+    RISING_MAINTAINER="Weaponmasterjax"
+
+# whether to ship aperture camera, default is false
+PRODUCT_NO_CAMERA := true
+
+TARGET_ENABLE_BLUR := true
+
+# Wether to ship lawnchair launcher
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_PREBUILT_PIXEL_LAUNCHER := false
+
+# GMS build flags
+# ship with GMS packages, replaces default AOSP packages with Google manufactured packages.
+WITH_GMS := true
+# These flags needs WITH_GMS set to true
+# for more information about core GMS flags, please see vendor/gms/common/common-vendor.mk
+# ships core GMS components that are needed to run GMS environment
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := false
+PRODUCT_PACKAGES += \
+    Photos \
+    MarkupGoogle \
+    LatinIMEGooglePrebuilt \
+    AiWallpapers \
+    WallpaperEmojiPrebuilt \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Velvet
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+RISING_MAINTAINER := Weaponmasterjax
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_MAINTAINER="Weaponmasterjax"		
+
 BUILD_FINGERPRINT := OnePlus/CPH2609/OP5D35L1:14/TP1A.220905.001/U.R4T3.16afb3d-1-aa4b:user/release-keys
